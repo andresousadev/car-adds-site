f5766fed16c2c988db76bd163b5f27a1bbde3e71
André Sousa <andresousa@MacBook-Pro-de-Andre.local>
2021-05-31 01:01:09 +0100

    Added graphql request to form with urql package

--- front/src/components/InputField.tsx
+++ front/src/components/InputField.tsx
@@ -9,17 +9,15 @@ import React, { InputHTMLAttributes } from "react";
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
-  placeholder: string;
   name: string;
-  type?: string;
 };
 
-export const InputField: React.FC<InputFieldProps> = (props) => {
+export const InputField: React.FC<InputFieldProps> = ({label, size: _, ...props}) => {
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
-      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-      <Input {...field} id={field.name} placeholder={props.placeholder} type={props.type ? props.type : 'text'}/>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
--- front/src/pages/register.tsx
+++ front/src/pages/register.tsx
@@ -1,32 +1,97 @@
 import React from "react";
 import { Form, Formik } from "formik";
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Input } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
+const REGISTER_MUT = `
+mutation Register(
+  $email: String!,
+  $firstName: String!,
+  $lastName: String!,
+  $password: String!,
+	$confirmPassword: String!){
+  registerUser(options: {
+    userEmail: $email,
+    firstName: $firstName,
+  	lastName: $lastName,
+  	password: $password,
+  	confirmPassword: $confirmPassword
+  }){
+    errors{field, message},
+    user{
+      id
+      createdAt
+      updatedAt
+      firstName
+      lastName
+      userEmail
+    }
+  }
+}
+`;
+
 const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUT);
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ email: "", password: "" }}
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }}
         onSubmit={(values) => {
           console.log(values);
+          return register(values);
         }}
       >
         {({ isSubmitting }) => (
           <Form>
-            <InputField name="email" placeholder="email" label="Email"/>
+            <InputField
+              name="firstName"
+              placeholder="First Name"
+              label="First Name"
+            />
+            <Box mt={4}>
+              <InputField
+                name="lastName"
+                placeholder="Last Name"
+                label="Last Name"
+              />
+            </Box>
+            <Box mt={4}>
+              <InputField name="email" placeholder="email" label="Email" />
+            </Box>
             <Box mt={4}>
               <InputField
                 name="password"
-                placeholder="password"
+                placeholder="Password"
                 label="Password"
                 type="password"
               />
             </Box>
-            <Button type="submit" colorScheme="teal" isLoading={isSubmitting} mt={4}>Register</Button>
+            <Box mt={4}>
+              <InputField
+                name="confirmPassword"
+                placeholder="Confirm Password"
+                label="Confirm Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              type="submit"
+              colorScheme="teal"
+              isLoading={isSubmitting}
+              mt={4}
+            >
+              Register
+            </Button>
           </Form>
         )}
       </Formik>
